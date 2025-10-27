8ef7aa2823251f4cfeab6f1f4c71955f98fbb7eb
Sérgio Henrique <dnxpollo@gmail.com>
2025-10-27 14:14:25 -0300

    criação do countdown, criação do formulário, organização dos input's

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { Heading } from './components/Heading';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo'
 import { Menu } from './components/Menu'
+import { CountDown} from './components/CountDown';
 
 import './styles/global.css';
 import './styles/theme.css';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
     console.log('oi');
@@ -18,6 +20,27 @@ export function App() {
 
             <Container>
                 <Menu/>
+            </Container>
+            <Container>
+                <CountDown/>
+            </Container>
+            <Container>
+                <form className='form' action=''>
+                    <div className='formRow'>
+                        <DefaultInput labelText='Task' id='meuInput' type='text' />
+                    </div>
+
+                    <div className='formRow'>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div className='formRow'>
+                        <p>Ciclos</p>
+                        <p>0 0 0 0 0 0 0</p>
+                    </div>
+                    <div className='formRow'>
+                        <button>Enviar</button>
+                    </div>
+                </form>
             </Container>
         </>
     )
